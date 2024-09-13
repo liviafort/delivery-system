11a2ba0039c7a4057625e33ed6fe9eeff502c4ef
liviafort <livia.fortunato@academico.ifpb.edu.br>
2024-09-13 09:44:01 -0300

    feat: spring boot configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.0"
 }
+
+
 
 group = "br.com.liviafort"
 version = "1.0-SNAPSHOT"
@@ -10,9 +15,10 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("io.mockk:mockk:1.13.11")
-    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
@@ -20,5 +26,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 }
