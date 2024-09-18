cebe0db44934f74cd51b87a2165fe9057186a3fb
liviafort <livia.fortunato@academico.ifpb.edu.br>
2024-09-18 13:26:46 -0300

    feat: corrections and spring implementation with notations

--- src/main/kotlin/br/com/liviafort/deliverysystem/App.kt
+++ src/main/kotlin/br/com/liviafort/deliverysystem/App.kt
@@ -6,54 +6,60 @@ import br.com.liviafort.deliverysystem.application.submenu.DeliverymanMenuOperat
 import br.com.liviafort.deliverysystem.application.submenu.OrderMenuOperations
 import br.com.liviafort.deliverysystem.application.submenu.RestaurantMenuOperations
 import br.com.liviafort.deliverysystem.application.submenu.RouteMenuOperations
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
-val restaurantMenu = RestaurantMenuOperations()
-val clientMenu = CustomerMenuOperations()
-val deliverymanMenu = DeliverymanMenuOperations()
-val orderMenu = OrderMenuOperations()
-val routeMenu = RouteMenuOperations()
+@SpringBootApplication
+class App : CommandLineRunner {
 
-fun main() {
-    ReusableComponents()
-    var selectedOption = selectMenuOption()
+    val restaurantMenu = RestaurantMenuOperations()
+    val clientMenu = CustomerMenuOperations()
+    val deliverymanMenu = DeliverymanMenuOperations()
+    val orderMenu = OrderMenuOperations()
+    val routeMenu = RouteMenuOperations()
 
-    while (selectedOption != "6") {
-        when (selectedOption) {
-            "1" -> {
-                clientMenu.menu()
+    override fun run(vararg args: String?) {
+        ReusableComponents()
+        var selectedOption = selectMenuOption()
+
+        while (selectedOption != "6") {
+            when (selectedOption) {
+                "1" -> {
+                    clientMenu.menu()
+                }
+                "2" -> {
+                    restaurantMenu.menu()
+                }
+                "3" -> {
+                    deliverymanMenu.menu()
+                }
+                "4" -> {
+                    orderMenu.menu()
+                }
+                "5" -> {
+                    routeMenu.menu()
+                }
+                else -> {
+                    println("Opção inválida")
+                }
             }
-
-            "2" -> {
-                restaurantMenu.menu()
-            }
-            "3" -> {
-                deliverymanMenu.menu()
-            }
-
-            "4" -> {
-                orderMenu.menu()
-            }
-
-            "5" -> {
-                routeMenu.menu()
-            }
-
-            else -> {
-                println("Opção inválida")
-            }
+            selectedOption = selectMenuOption()
         }
-        selectedOption = selectMenuOption()
+    }
+
+    private fun selectMenuOption(): String {
+        println("Menu")
+        println("1 - Operações de cliente")
+        println("2 - Operações de restaurante")
+        println("3 - Operações de entregador")
+        println("4 - Fazer pedido")
+        println("5 - Operações de rota")
+        println("6 - Sair")
+        return readlnOrNull() ?: "6"
     }
 }
 
-
-private fun selectMenuOption(): String {
-    println("Menu")
-    println("1 - Operações de cliente")
-    println("2 - Operações de restaurante")
-    println("3 - Operações de entregador")
-    println("4 - Fazer pedido")
-    println("5 - Operações de rota")
-    println("6 - Sair")
-    return readlnOrNull() ?: "6"
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
 }
